5f32db69cb25cd6c7c66939d60ebb9d82237fe31
Leonardo Lira Guimarães <do.leonardo.lira@gmail.com>
2021-03-31 12:29:04 -0400

    [API] - fix warning for equals and hashcode not in composite-key class
    #1117

--- src/main/java/org/ipdec/marfim/api/model/key/RolePermissionKey.java
+++ src/main/java/org/ipdec/marfim/api/model/key/RolePermissionKey.java
@@ -3,6 +3,7 @@ package org.ipdec.marfim.api.model.key;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 
 @Embeddable
@@ -13,4 +14,16 @@ public class RolePermissionKey implements Serializable {
     @Column(name = "permission_code")
     private String permissionCode;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RolePermissionKey)) return false;
+        RolePermissionKey that = (RolePermissionKey) o;
+        return roleId.equals(that.roleId) && permissionCode.equals(that.permissionCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleId, permissionCode);
+    }
 }

b53b5970be7cb5dd65f69a26eace627538324cbf
Leonardo Lira Guimarães <do.leonardo.lira@gmail.com>
2021-05-13 10:23:57 -0400

    [API] [WEB] - Removing users and permissions from roles crud
    #1678, #1686

--- src/main/java/org/ipdec/marfim/api/dto/CreateRoleDTO.java
+++ src/main/java/org/ipdec/marfim/api/dto/CreateRoleDTO.java
@@ -6,12 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.ipdec.marfim.api.model.*;
-import org.ipdec.marfim.api.model.key.PermissionKey;
 
 import javax.validation.constraints.NotBlank;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -24,16 +20,12 @@ public class CreateRoleDTO {
     private String name;
     private String description;
     private Boolean isAdmin;
-    private List<PermissionDTO> permissions;
-    private List<UserDTO> users;
 
     public CreateRoleDTO(Role role) {
         id = role.getId();
         name = role.getName();
         description = role.getDescription();
         isAdmin = role.getIsAdmin();
-        permissions = role.getPermissions().stream().map(PermissionDTO::new).collect(Collectors.toList());
-        users = role.getUsers().stream().map(UserDTO::new).collect(Collectors.toList());
     }
 
     public Role parseToRole(Role role) {
@@ -43,28 +35,6 @@ public class CreateRoleDTO {
         role.setName(name);
         role.setDescription(description);
         role.setIsAdmin(isAdmin);
-
-        if(users!=null) {
-            List<User> usersList = users.stream().map(userDTO -> {
-                User user = new User();
-                user.setId(userDTO.getId());
-                return user;
-            }).collect(Collectors.toList());
-
-            role.setUsers(usersList);
-        }
-
-        if(permissions!=null){
-            Set<Permission> permissionsList = permissions.stream()
-                    .map(permissionDTO -> {
-                        PermissionKey key = new PermissionKey(permissionDTO.getResourceCode(), permissionDTO.getLevelCode());
-                        Permission permission = new Permission();
-                        permission.setId(key);
-                        return permission;
-                    })
-                    .collect(Collectors.toSet());
-            role.setPermissions(permissionsList);
-        }
 
         return role;
     }
